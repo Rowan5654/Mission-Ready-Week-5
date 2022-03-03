1f30cf2bcfc0162a7ba7456713dd437cd95bd2a7
Rowan Craik <Rowan5654@hotmail.com>
2022-03-03 15:28:53 +1300

    fixed CounterComponent syntax

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
         <div>counter: {{counter}}</div>
-        <button @click="IncrementCounter">Increment</button>
+        <button @click="IncrementCounter()">Increment</button>
     </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    counter: 0,
+  data () {
+    return {
+      counter: 0
+    }
+  },
   methods: {
-    IncrementCounter() {
-      counter += 1
+    IncrementCounter () {
+      this.counter += 1
     }
   }
-  })
 }
 </script>
